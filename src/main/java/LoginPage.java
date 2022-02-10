bc3fb2c9c0253ee9b91f19fa3e1d8a1f25c5a8a6
wissem <machraoui.wissem@gmail.com>
2022-02-10 14:05:01 +0100

    Test de deconexion+verification des élements de la page my account

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -11,11 +11,14 @@ public class LoginPage {
     By password = By.cssSelector("#password");
     By ButtonLoginselector =By.cssSelector("[name=login]");
     By cnxok = By.cssSelector(".woocommerce-MyAccount-content > p:nth-child(1)");
+    By reconectbutton = By.cssSelector("p.form-row:nth-child(3) > input:nth-child(3)");
+    By conneexionwithemptyMail = By.cssSelector(".woocommerce-error > li:nth-child(1)");
     long timeout = 10 ;
 
     public LoginPage(WebDriver driver){
         this.driver=driver;
     }
+
     public LoginPage LoginOk (String login , String Password) {
 
         driver.findElement(usernameselector).sendKeys(login);
@@ -28,6 +31,27 @@ public class LoginPage {
     }
     public String conxOK () {
        return driver.findElement((cnxok)).getText() ;
+    }
+
+
+    public MyAccountPage Login_AccountPage (String login , String Password) {
+
+        driver.findElement(usernameselector).sendKeys(login);
+        driver.findElement(password).sendKeys(Password);
+        driver.findElement(ButtonLoginselector).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(cnxok));
+
+        MyAccountPage myAccountPage = new MyAccountPage(driver);
+        return myAccountPage ;
+    }
+    public String Logout_succesful (){
+
+        driver.findElement(reconectbutton).click();
+       return  driver.findElement(conneexionwithemptyMail).getText();
+
+
+
     }
 
 
--- src/main/java/MyAccountPage.java
+++ src/main/java/MyAccountPage.java
@@ -1,2 +1,67 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class MyAccountPage {
+    WebDriver driver ;
+    By Dashboardselector = By.cssSelector("li.woocommerce-MyAccount-navigation-link:nth-child(1) > a:nth-child(1)");
+    By orderLinkSelector = By.cssSelector("li[class='woocommerce-MyAccount-navigation-link woocommerce-MyAccount-navigation-link--orders'] a");
+    By downloadLinkSelector = By.cssSelector("li[class='woocommerce-MyAccount-navigation-link woocommerce-MyAccount-navigation-link--downloads'] a");
+    By adressesLinkSelector = By.cssSelector("a[href='http://practice.automationtesting.in/my-account/edit-address/']");
+    By accountdetailsSelector = By.cssSelector("a[href='http://practice.automationtesting.in/my-account/edit-account/']");
+    By logOutSelector = By.cssSelector("li.woocommerce-MyAccount-navigation-link:nth-child(6) > a:nth-child(1)");
+
+    long timeout =10 ;
+
+
+    public MyAccountPage (WebDriver driver){
+        this.driver=driver;
+    }
+
+    public String getDashbord (){
+        return driver.findElement(Dashboardselector).getText();
+
+    }
+    public String logOutSelector (){
+
+        return driver.findElement(logOutSelector).getText();
+
+    }
+    public String adressesLinkSelector (){
+
+        return driver.findElement(adressesLinkSelector).getText();
+
+    }
+    public String downloadLinkSelector (){
+
+        return driver.findElement(downloadLinkSelector).getText();
+
+    }
+    public String orderLinkSelector (){
+
+        return driver.findElement(orderLinkSelector).getText();
+
+    }
+
+public LoginPage Logout_Valid (){
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+    wait.until(ExpectedConditions.elementToBeClickable(logOutSelector));
+
+    driver.findElement(logOutSelector).click();
+
+    LoginPage loginPage = new LoginPage(driver);
+    return loginPage ;
+
+}
+
+
+
+
+
+
 }
